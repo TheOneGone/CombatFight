03774791eba5b36d31ebd6d99acaa312eb6b869a
Bekir <bekirshabut66@gmail.com>
2018-06-03 14:17:20 +0300

    Added RadioInterface component and test

---
+++ static/components/RadioInterface/RadioInterface.js
@@ -0,0 +1,31 @@
+class RadioInterface{
+    constructor(name, fields, root, className){
+        this.buttons = [];
+        for (let field in fields){
+            let radioLabel = document.createElement("label");
+            radioLabel.setAttribute("for", field);
+            radioLabel.innerText = field;
+            root.appendChild(radioLabel);
+            let radioButton = document.createElement("input");
+            radioButton.setAttribute("type", "radio");
+            radioButton.setAttribute("name", name);
+            radioButton.setAttribute("id", field);
+            if(className){
+                radioButton.setAttribute("class", className);
+            }
+            radioButton.setAttribute("value", fields[field]);
+            root.appendChild(radioButton);
+            this.buttons.push(radioButton);
+        }
+        this.buttons[0].checked = true;
+    }
+
+    value(){
+        for(let button of this.buttons){
+            if(button.checked){
+                return button.value;
+            }
+        }
+        return undefined;
+    }
+}
