c0452179321d63e6522b2d8ad88ebe0f9b34dcac
Alexandr Shleiko <dusty@iMac-Alexandr.local>
2018-06-03 16:17:35 +0300

    бойцу добавлен метод badge() и тесты для него

--- static/components/fighter/fighter.js
+++ static/components/fighter/fighter.js
@@ -7,4 +7,11 @@ function FighterInfo (userInfoJSON) {
     this.lastActive =  userInfo.last_active;
     this.health = userInfo.health;
     this.token = userInfo.token;
+
+    this.badge = function () {
+        var a = document.createElement('a');
+        a.innerText = this.userName;
+        a.href = 'user.html?id=' + this.id;
+        return a;
+    }
 }
