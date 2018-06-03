3e897503f120c707708eb02c71ed563876ee3680
Alexandr Shleiko <dusty@iMac-Alexandr.local>
2018-06-03 16:41:20 +0300

    создание онлайна v0.1

--- static/components/online/online.js
+++ static/components/online/online.js
@@ -1,3 +1,8 @@
-function Online (fighters) {
-    
+function Online (node, fighters) {
+    if (this.fighters !== fighters) {
+        this.fighters = fighters
+    }
+    this.refresh = function () {
+        
+    }
 }
