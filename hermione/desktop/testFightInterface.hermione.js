6b966995e0d6d8cdebcb2a248a15f21e57b133ae
TheOneGone <dmit.ataeff@yandex.ru>
2018-06-10 15:47:03 +0300

    FightInterface:Добавлен hermione тест

---
+++ hermione/desktop/testFightInterface.hermione.js
@@ -0,0 +1,21 @@
+const assert = require('chai').assert;
+
+describe('Сценарий боя', function () {
+    beforeEach(function() {
+        return this.browser
+            .url('http://localhost:3333/FightFormInterface.html');
+    });
+ 
+    it('Выводит результат если выбрана верная последовательность',function () {
+        const expectedText = 'Hit: 2\nBlock: 3, 4';
+
+        return this.browser
+            .click('.fight-form label[for=Torso]')
+            .click('.fight-form label[for=Legs-Head]')
+            .click('input[type=submit]')
+            .alertText()
+            .then(function (text) {
+                assert.equal(text, expectedText);
+            });           
+    });
+});
