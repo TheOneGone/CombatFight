5ba5da67e1cb71f54d33fd0cd72efe1ae7939270
Bekir <bekirshabut66@gmail.com>
2018-06-10 17:04:23 +0300

    fixed fight form interface

--- static/components/FightFormInterface/FightFormInterface.js
+++ static/components/FightFormInterface/FightFormInterface.js
@@ -1,9 +1,13 @@
 class FightFormInterface{
     constructor(formDOM, radios, submitButtonInfo, onSubmit){
         this.radioIntrefaces = [];
+        let i = 0;
         for(let radio of radios){
             let buttonsDiv = document.createElement("div");
             buttonsDiv.classList.add("radio-buttons");
+            buttonsDiv.classList.add("radio-buttons-" + i);
+            buttonsDiv.classList.add("radio-div-" + i);
+            i++;
             formDOM.appendChild(buttonsDiv);
             let radioInterface = new RadioInterface(radio.name, radio.fields, buttonsDiv, radio.className);
             this.radioIntrefaces.push(radioInterface);
